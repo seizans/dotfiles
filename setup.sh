0c99c2663fd22f10ac1f88d7980d494c1997eb23
Seizan Shimazaki <seizans@gmail.com>
2016-06-15 12:48:41 +0900

    Fix

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ cd ~
 brew install neovim/neovim/neovim
 mkdir .vim
 mkdir .config
-ln -sv .vim .config/nvim
+ln -sv .config/nvim .vim
 mkdir -p ~/.vim/dein
 mkdir -p ~/.vim/colors
 
@@ -12,6 +12,5 @@ sh installer.sh ~/.config/nvim/dein
 
 ln -snvf dotfiles/nvimrc .vimrc
 ln -snvf dotfiles/dein.toml .dein.toml
-ln -svnf dotfiles/dein_lazy.toml .dein_lazy.toml
 
 git config --global include.path ~/dotfiles/gitconfig
