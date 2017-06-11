9a1bad4d80a371ca8252b585b7689d82b7af2a8c
Seizan Shimazaki <seizan.shimazaki@aktsk.jp>
2017-06-11 09:33:35 +0900

    少しなおすぞ

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 cd ~
 
-brew install neovim/neovim/neovim
+brew tap neovim/neovim
+brew install neovim
+
 mkdir -p ~/.config/nvim/dein
 mkdir -p ~/.config/nvim/colors
 
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh installer.sh ~/.config/nvim/dein
 
-ln -snvf dotfiles/nvimrc .config/nvim/init.vim
-ln -snvf dotfiles/dein.toml .config/nvim/dein.toml
+ln -snvf ~/dotfiles/nvimrc .config/nvim/init.vim
+ln -snvf ~/dotfiles/dein.toml .config/nvim/dein.toml
 
 git config --global include.path ~/dotfiles/gitconfig
